0e5ee67ca4f00bca4284fef39aa15b2c062be8a7
Oscar Martinez German <OscarMartinezGerman@hotmail.com>
2025-05-08 08:54:55 -0400

    Initial Commit

---
+++ TVMaze/TVMazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeApp.swift
+//  TVMaze
+//
+//  Created by Oscar Martínez Germán on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
