484981a397a4188be181c196450b048b8415c497
Oscar Martinez German <OscarMartinezGerman@hotmail.com>
2025-05-08 19:41:29 -0400

    Added Poster and Rating model

---
+++ TVMaze/src/Model/Poster.swift
@@ -0,0 +1,13 @@
+//
+//  Poster.swift
+//  TVMaze
+//
+//  Created by Oscar Martínez Germán on 8/5/25.
+//  Copyright © 2025 TMaze Demo. All rights reserved.
+//
+
+import Foundation
+
+public struct Poster: Codable {
+    let medium, original: String
+}
---
+++ TVMaze/src/Model/Rating.swift
@@ -0,0 +1,14 @@
+//
+//  Rating.swift
+//  TVMaze
+//
+//  Created by Oscar Martínez Germán on 8/5/25.
+//  Copyright © 2025 TMaze Demo. All rights reserved.
+//
+
+import Foundation
+
+public struct Rating: Codable {
+    let average: Double?
+}
+
